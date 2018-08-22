0c7266880be34a027ed6c2696f51bfe3de3e5d58
arianna haradon <ariannaharadon@gmail.com>
2018-08-22 16:09:56 -0500

    add scripts to run frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import logger from 'morgan';
-import mongoose from 'mongoose';
-
+const express = require('express');
 const app = express();
 const router = express.Router();
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const API_PORT = process.env.API_PORT || 3000;
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
@@ -18,3 +18,13 @@ router.get('/', (req, res) => {
 app.use('/api', router);
 
 app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
+
+
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
+//
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
