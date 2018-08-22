d73bd63b7341a8e7d663542b1d40c3754d65f794
arianna haradon <ariannaharadon@gmail.com>
2018-08-22 16:51:45 -0500

    wip mongoose queries

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,33 @@ app.use('/api', router);
 
 app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
 
+router.get('/', (req, res) => {
+  User.find({})
+    .exec((err, devices) => {
+      if (err) reject(err)
+      resolve(devices)
+    })
+}
+});
+
+router.post('/comments', (req, res) => {
+  const comment = new Comment();
+  // body parser lets us use the req.body
+  const { author, text } = req.body;
+  if (!author || !text) {
+    // we should throw an error. we can do this check on the front end
+    return res.json({
+      success: false,
+      error: 'You must provide an author and comment'
+    });
+  }
+  comment.author = author;
+  comment.text = text;
+  comment.save(err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
 
 // app.use(function (req, res, next) {
 //   res.header('Access-Control-Allow-Origin', '*');
